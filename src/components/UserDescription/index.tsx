1591c8cdcf4f61b1dc5650afe6ae0134215efb04
koi2000 <1754520660@qq.com>
2022-11-26 20:32:49 +0800

    修复地图bug，修改样式

--- src/components/UserDescription/index.tsx
+++ src/components/UserDescription/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Descriptions} from 'antd';
+import {Alumnus} from "../Modal";
+import data from "../../static/icon/Title/data";
 
 
 interface propsType {
@@ -7,18 +9,15 @@ interface propsType {
     isModalOpen?: boolean;
     // 市
     city?: string;
+    user: Alumnus
 }
 
 export default function UserDescription(props: propsType) {
     return (
-        <Descriptions title="User Info">
-            <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
-            <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-            <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-            <Descriptions.Item label="Remark">empty</Descriptions.Item>
-            <Descriptions.Item label="Address">
-                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-            </Descriptions.Item>
+        <Descriptions >
+            <Descriptions.Item label="姓名">{props.user.name}</Descriptions.Item>
+            <Descriptions.Item label="电话">{props.user.phone}</Descriptions.Item>
+            <Descriptions.Item label="邮箱">{props.user.email}</Descriptions.Item>
         </Descriptions>
     )
 };
